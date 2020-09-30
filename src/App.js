c6e9d5905ac43e50dd6de8421b7c14ac190a0802
ManuelCanoFig <jcano@ucol.mx>
2020-09-30 12:14:53 -0500

    Redux set up. Deleted classes unused

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import React, {useEffect} from 'react';
+import React, {Fragment, useEffect} from 'react';
 import 'materialize-css/dist/css/materialize.min.css'
 import M from 'materialize-css/dist/js/materialize.min.js'
+
+//import libraries to work with redux
+import {Provider} from 'react-redux'
+import store from './store'
+
 
 import TechListModal from './components/techs/TechListModal'
 import EditLogModal from './components/logs/EditLogModal'
@@ -17,19 +22,21 @@ const App = () => {
   });
 
   return (
-
-
-    <div className="App">
-      <SearchBar/>
-      <div className='container'>
-        <AddBtn/>
-        <AddLogModal/>
-        <EditLogModal/>
-        <AddTechModal/>
-        <TechListModal/>
-        <Logs/>
-      </div>
-    </div>
+    <Provider store={store}>
+      <Fragment>
+        <div className="App">
+          <SearchBar/>
+          <div className='container'>
+            <AddBtn/>
+            <AddLogModal/>
+            <EditLogModal/>
+            <AddTechModal/>
+            <TechListModal/>
+            <Logs/>
+          </div>
+        </div>
+      </Fragment>
+    </Provider>
   );
 }
 
